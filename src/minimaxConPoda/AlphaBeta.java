cf81d43c29c54f0b77955f988187967ce81c1225
Gerardo Fac <34481624+yerfac98@users.noreply.github.com>
2020-10-30 07:57:45 -0600

    minimaxYpoda

---
+++ src/minimaxConPoda/AlphaBeta.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minimaxConPoda;
+
+/**
+ *
+ * @author Gerardo Fac
+ */
+public class AlphaBeta {
+    
+}
